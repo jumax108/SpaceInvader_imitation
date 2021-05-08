4f7f7b0ed5e9a405feff81fa251cb2653723351c
jumax108 <jumax108@kakao.com>
2021-05-08 16:39:20 +0900

    Add SourceFile

---
+++ pjt_shot/shot/GameScene.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Windows.h>
+#include <stdio.h>
+#include "myStack.h"
+#include "Console.h"
+#include "enumForMsg.h"
+
+
+// Game Scene Logic
+void updateGame();
+// Game Scene render
+void renderGame();
---
+++ pjt_shot/shot/TitleScene.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Windows.h>
+#include "myStack.h"
+#include "Console.h"
+#include "enumForMsg.h"
+
+// Title Scene Logic
+void updateTitle();
+// Title Scene render
+void renderTitle();
